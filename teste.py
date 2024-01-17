32bcb5f7716923133acc6f4542bc8a18ffc69e66
Felipe Leite <felipesd.leite@gmail.com>
2024-01-17 15:42:03 -0300

    chore: adding type functions

--- teste.py
+++ teste.py
@@ -10,3 +10,9 @@ and can be used to made multi line comments
 
 print(12, 34, sep=" | ", end=" ") #SEP IS USED TO SEPARATE THE VALUES WITH A SPECIAL SYMBOL
 print(56, 78, sep=' ')
+print(type("ola"))
+print(type(-1))
+print(type(1.1))
+
+#type() can be used to show the class type of the object
+#Everything in python is a object
